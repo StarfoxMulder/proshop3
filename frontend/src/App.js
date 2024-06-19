4729d0cc58b6dcb382690a9ea2bc9d414a86005a
StarfoxMulder <cjprestia@gmail.com>
2024-06-18 20:28:49 -0600

    Worked on initial product screen and route; from the home page if you clikc on an item you will be routed to a view of that item with image, name, reviews, and price

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <main className="py-3">
         <Container>
           <h2>Welcome to ProShop</h2>
+          <Outlet />
         </Container>
       </main>
       <Footer />
