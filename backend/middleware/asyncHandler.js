6144c8ae2deee8c59f2da89ae619954d68255313
StarfoxMulder <cjprestia@gmail.com>
2024-06-24 20:51:59 -0600

    Added newlines to end of files I missed before. STarted to add Redux. Added controller file for Product routes.

--- backend/middleware/asyncHandler.js
+++ backend/middleware/asyncHandler.js
@@ -3,4 +3,5 @@ const asyncHandler = fn => (req, res, next) => {
   // Takes in the request, if the promise resolves it's going to call next
   // which calls the next piece of middleware
 };
+
 export default asyncHandler;
