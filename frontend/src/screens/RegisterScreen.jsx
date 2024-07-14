4aec2a51eaccc6e34c32e4ac7d25e65493534567
StarfoxMulder <cjprestia@gmail.com>
2024-07-14 15:06:01 -0600

    Added registration screen for new users; validation and handling of new user data; handling of log out request in header

--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -4,18 +4,20 @@ import { Form, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from "../components/FormContainer"
 import Loader from "../components/Loader"
-import { useLoginMutation } from "../slices/usersApiSlice"
+import { useRegisterMutation } from '../slices/usersApiSlice'
 import { setCredentials } from '../slices/authSlice'
 import { toast } from 'react-toastify'
 
-const LoginScreen = () => {
+const RegisterScreen = () => {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const [login, { isLoading }] = useLoginMutation();
+  const [register, { isLoading }] = useRegisterMutation();
   // Getting userInfo from the state
   const { userInfo } = useSelector((state) => state.auth)
 
@@ -35,28 +37,41 @@ const LoginScreen = () => {
     e.preventDefault();
     console.trace();
     console.log(e);
-    try {
-      const res = await login({ email, password }).unwrap();
-      dispatch(setCredentials({ ...res }));
-      navigate(redirect);
-    } catch (error) {
-      console.log(error);
-      toast.error(error?.data?.message || error?.error);
+    if (password !== confirmPassword) {
+      toast.error('Passwords do not match');
+    } else {
+      try {
+        const res = await register({ name, email, password }).unwrap();
+        dispatch(setCredentials({ ...res }));
+        navigate(redirect);
+      } catch (error) {
+        console.log(error);
+        toast.error(error?.data?.message || error?.error);
+      }
     }
   };
 
   return (
     <FormContainer>
-      <h1>Sign In</h1>
+      <h1>Sign Up</h1>
 
       <Form onSubmit={submitHandler}>
+        <Form.Group controlId='name' className="my-3">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
         <Form.Group controlId='email' className="my-3">
           <Form.Label>Email Address</Form.Label>
           <Form.Control
-          type='email'
-          placeholder='Enter email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+            type='email'
+            placeholder='Enter email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           ></Form.Control>
         </Form.Group>
         <Form.Group controlId='password' className="my-3">
@@ -68,6 +83,15 @@ const LoginScreen = () => {
           onChange={(e) => setPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
+        <Form.Group controlId='confirmPassword' className="my-3">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Enter password again'
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
         <Button
           type='submit'
           variant='primary'
@@ -75,16 +99,15 @@ const LoginScreen = () => {
           onClick={submitHandler}
           disabled={ isLoading }
         >
-          Sign In
+          Register
         </Button>
         {isLoading && <Loader />}
       </Form>
-
       <Row className='py-3'>
         <Col>
-        New customer?{' '}
-        <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
-        Register
+        Already have an account?{' '}
+        <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>
+        Log in
         </Link>
         </Col>
       </Row>
@@ -92,4 +115,4 @@ const LoginScreen = () => {
   )
 }
 
-export default LoginScreen
+export default RegisterScreen
--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -8,9 +8,26 @@ export const usersApiSlice = apiSlice.injectEndpoints({
         url: `${USERS_URL}/login`,
         method: 'POST',
         body: data,
-      }),
+      })
+    }),
+    register: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}`,
+        method: 'POST',
+        body: data
+      })
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/logout`,
+        method: 'POST'
+      })
     })
   })
 });
 
-export const { useLoginMutation } = usersApiSlice;
+export const {
+  useLoginMutation,
+  useRegisterMutation,
+  useLogoutMutation
+} = usersApiSlice;
