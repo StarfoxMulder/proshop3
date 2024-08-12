6c5422ffcdeae286efdf1d75f1fb1dafec72663c
StarfoxMulder <cjprestia@gmail.com>
2024-08-12 17:08:12 -0600

    Made progress on the form for updating a product's details

--- frontend/src/screens/ProductEditScreen.jsx
+++ frontend/src/screens/ProductEditScreen.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, FormGroup } from 'react-bootstrap';
+import { FaArrowLeft } from 'react-icons/fa';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import FormContainer from '../components/FormContainer';
@@ -20,11 +21,89 @@ const ProductEditScreen = () => {
 
   const { data: product, isLoading, refetch, error } = useGetProductDetailsQuery(productId) 
 
-  console.log(product);
+  const [updateProduct, { isLoading: loadingUpdate}] = useUpdateProductMutation()
 
-  return (
-    <div>ProductEditScreen</div>
-  )
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (product) {
+      setName(product.name);
+      setPrice(product.price);
+      setimage(product.image);
+      setBrand(product.brand);
+      setCategory(product.category);
+      setCountInStock(product.countInStock);
+      setDescription(product.description);
+    } else {
+
+    }
+  }, [product]);
+
+  return <>
+    <Link to='/admin/productList' className='btn btn-light my-3'>
+      <FaArrowLeft /> Go Back
+    </Link>
+    <FormContainer>
+      <h1>Edit Product</h1>
+      { loadingUpdate && <Loader /> }
+      { isLoading ? <Loader />
+        : error ? <Message variant='danger'>{error}</Message>
+        : (
+          <Form>
+            <Form.Group controlId='name' className='my-2'>
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId='price' className='my-2'>
+              <Form.Label>Price</Form.Label>
+              <Form.Control
+                type='number'
+                placeholder='Enter price'
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+           {/* Image input */}
+
+            <Form.Group controlId='brand' className='my-2'>
+              <Form.Label>Brand</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter brand'
+                value={brand}
+                onChange={(e) => setBrand(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId='category' className='my-2'>
+              <Form.Label>Category</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter category'
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId='countInStock' className='my-2'>
+              <Form.Label>Count in Stock</Form.Label>
+              <Form.Control
+                type='number'
+                placeholder='Enter count in stock'
+                value={countInStock}
+                onChange={(e) => setCountInStock(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+          </Form>
+        )
+      }
+    </FormContainer>
+  </>
+  
 }
 
 export default ProductEditScreen
